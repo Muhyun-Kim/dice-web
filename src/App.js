4a3faf6c1a5383164b28e6244de96f8bb1649589
My Name <azs030369@gmail.com>
2022-12-29 22:56:19 +0900

    func for dice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import styles from "./App.module.css";
 import Title from "./js/Title";
 import DiceMain from "./js/DiceMain";
-import Btn from "./js/Btn";
 
 function App() {
   return (
     <div className={styles.App}>
       <Title />
       <DiceMain />
-      <Btn />
     </div>
   );
 }
--- src/js/DiceMain.js
+++ src/js/DiceMain.js
@@ -1,11 +1,18 @@
 import styles from "../css/DiceMain.module.css";
-import Btn from "./Btn"
-import {str} from './Btn'
+import { useState } from "react";
 
 function DiceMain() {
+  //サイコロに表示されるfunctionを作る
+  let valueDice = [1, 2, 3, 4, 5, 6];
+  let pickDice = Math.floor(Math.random() * valueDice.length);
+  const [dice, setDice] = useState(valueDice[0]);
+  const rollDice = () => setDice((prev) => prev + 1);
   return (
     <div className={styles.DiceMain}>
-      <span className={styles.DiceValue}>{str}</span>
+      <span className={styles.DiceValue}>{dice}</span>
+      <button onClick={rollDice} className={styles.Btn}>
+        rolldice
+      </button>
     </div>
   );
 }
